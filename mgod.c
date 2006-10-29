ba96d7ea447e38d2c232498f92363e668d60748a
kzed <kzed@bce501d2-971f-0410-8e5e-b64c4d416e7d>
2006-10-29 11:58:25 +0000

    adding support for gopher+
    git-svn-id: https://port70.net:1443/svn/mgod/mgod@5 bce501d2-971f-0410-8e5e-b64c4d416e7d

--- mgod.c
+++ mgod.c
@@ -1,4 +1,4 @@
-/* mgod 0.1a
+/* mgod 0.2
  * mini gopher server for inetd
  * by Mate Nagy <k-zed@hactar.net>
  * GPL VERSION 2
@@ -10,12 +10,15 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
 #include <strings.h>
 #include <dirent.h>
-
+#include <time.h>
+#include <stdarg.h>
 
 /* size of maximum request string and config file line */
 #define REQBUF 512
@@ -35,6 +38,8 @@
 char * servername = "127.0.0.1";
 int serverport = 70;
 char * rootdir = "/var/gopher";
+char * adminstring = "Frodo Gophermeister <fng@bogus.edu>";
+char * logfile = NULL;
 
 /* structure for keeping current path */
 typedef struct node node;
@@ -55,6 +60,27 @@ node *mkpn(const char *tx) {
 
 /* environment */
 char **genvp;
+
+/* append to log */
+void logprintf(const char *format, ...)
+{
+	FILE *fp;
+	va_list ap;
+
+	if(!logfile) return;
+
+	fp = fopen(logfile, "a");
+	if(!fp) {
+		perror("can't open log file");
+		return;
+	}
+
+	va_start(ap, format);
+	vfprintf(fp, format, ap);
+	va_end(ap);
+
+	fclose(fp);
+}
 
 /* helper functions for directory listing */
 int dirfilter(const struct dirent *a)
@@ -84,17 +110,43 @@ int dirsort(const struct dirent **a, const struct dirent **b)
 	}
 }
 
+/* gopher+ mode */
+int gopherplus = 0;
+/* print item info instead of file serve */
+int iteminfo = 0;
+/* process external processor's output */
+int managedextern = 0;
+
+/* file list generation properties */
+int genlist = 1;
+int listrev = 0;
+
+/* print error message */
+void errormsg(const char *e)
+{
+	if(gopherplus)
+		printf("--2\r\n%s\r\n", e);
+	else
+		printf("3%s\t\t\t\r\n.\r\n", e);
+}
+
 /* print directory entry */
 void printentry(const char *e)
 {
 	struct stat stbuf;
 	char *ext;
+	char *p, *q;
 	node *no;
+	char *ctype = NULL;
+	struct tm *mt;
 
 	if(stat(e, &stbuf)) {
-		printf("ican't stat '%s'\t\t\r\n", e);
+		errormsg("can't stat file");
 		return;
 	}
+
+	if(gopherplus)
+		fputs("+INFO: ", stdout);
 
 	if(S_ISDIR(stbuf.st_mode)) {
 		putchar('1');
@@ -105,37 +157,43 @@ void printentry(const char *e)
 			ext++;
 
 			/* images */
-			if(!strcasecmp(ext, "gif"))
-				putchar('g');
-			else if(!strcasecmp(ext, "jpg"))
-				putchar('I');
-			else if(!strcasecmp(ext, "jpeg"))
-				putchar('I');
-			else if(!strcasecmp(ext, "jpe"))
-				putchar('I');
-			else if(!strcasecmp(ext, "png"))
-				putchar('I');
-			else if(!strcasecmp(ext, "bmp"))
-				putchar('I');
+			if(!strcasecmp(ext, "gif")) {
+				putchar('g'); ctype="image/gif";
+			} else if(!strcasecmp(ext, "jpg")) {
+				putchar('I'); ctype="image/jpeg";
+			} else if(!strcasecmp(ext, "jpeg")) {
+				putchar('I'); ctype="image/jpeg";
+			} else if(!strcasecmp(ext, "jpe")) {
+				putchar('I'); ctype="image/jpeg";
+			} else if(!strcasecmp(ext, "png")) {
+				putchar('I'); ctype="image/png";
+			} else if(!strcasecmp(ext, "bmp")) {
+				putchar('I'); ctype="image/x-ms-bmp";
 
 			/* binary formats */
-			else if(!strcasecmp(ext, "gz"))
-				putchar('9');
-			else if(!strcasecmp(ext, "tgz"))
-				putchar('9');
-			else if(!strcasecmp(ext, "tar"))
-				putchar('9');
-			else if(!strcasecmp(ext, "zip"))
-				putchar('9');
+			} else if(!strcasecmp(ext, "gz")) {
+				putchar('9'); ctype="application/x-gtar";
+			} else if(!strcasecmp(ext, "tgz")) {
+				putchar('9'); ctype="application/x-gtar";
+			} else if(!strcasecmp(ext, "tar")) {
+				putchar('9'); ctype="application/x-tar";
+			} else if(!strcasecmp(ext, "zip")) {
+				putchar('9'); ctype="application/zip";
 
-			else if(!strcasecmp(ext, "html"))
-				putchar('h');
-			else if(!strcasecmp(ext, "htm"))
-				putchar('h');
+			} else if(!strcasecmp(ext, "html")) {
+				putchar('h'); ctype="text/html";
+			} else if(!strcasecmp(ext, "htm")) {
+				putchar('h'); ctype="text/html";
 
-			else putchar('0');
+			} else {
+				putchar('0');
+				ctype="text/plain";
+			}
 
-		} else putchar('0');
+		} else {
+			putchar('0');
+			ctype="text/plain";
+		}
 
 	} else {
 		return;
@@ -147,63 +205,125 @@ void printentry(const char *e)
 		printf("%s/", no->text);
 
 	printf("%s\t%s\t%d\t+\r\n", e, servername, serverport);
+
+	if(gopherplus) {
+		/* print admin block */
+		fputs("+ADMIN:\r\n", stdout);
+		printf(" Admin: %s\r\n", adminstring);
+		p = ctime(&stbuf.st_mtime);
+		q = strchr(p, '\n');
+		if(q) *q = 0;
+		mt = localtime(&stbuf.st_mtime);
+		
+		printf(" Mod-Date: %s <%04d%02d%02d%02d%02d%02d>\r\n",
+				p, mt->tm_year + 1900, mt->tm_mon + 1, mt->tm_mday,
+				mt->tm_hour, mt->tm_min, mt->tm_sec);
+
+		if(ctype) {
+			printf("+VIEWS:\r\n %s: <%ldK>\r\n", ctype, stbuf.st_size / 1024);
+		}
+	}
+}
+
+/* guess a VIEWS block from a link */
+void guessviews(const char *l)
+{
+	char *ctype = NULL;
+	switch(l[0]) {
+		case 'g': ctype = "image/gif"; break;
+		case 'I': ctype = "image/png"; break;
+		case '0': ctype = "text/plain"; break;
+		case '1':
+		case '7':
+				  break;
+		case 'h':
+				  ctype = "text/html"; break;
+		default:
+				  ctype = "application/octet-stream";
+	}
+
+	printf("+VIEWS:\r\n %s: <0K>\r\n", ctype);
+}
+
+/* process configuration file */
+void readdirlist(FILE *fp) {
+	char buf[REQBUF];
+	char *p, *cmd;
+
+	while(!feof(fp)) {
+		if(!fgets(buf, REQBUF, fp)) break;
+		if(ferror(fp)) break;
+		if(ferror(stdout)) break;
+
+		for(p=buf; *p; p++)
+			if(*p == '\r' || *p == '\n')
+				{ *p = 0; break; }
+
+		switch(buf[0]) {
+			case ':':
+				if(gopherplus)
+					fputs("+INFO: ", stdout);
+				fputs(buf+1, stdout);
+				fputs("\t+\r\n", stdout);
+				if(gopherplus) {
+					printf("+ADMIN:\r\n Admin: %s\r\n", adminstring);
+					guessviews(buf+1);
+				}
+				break;
+
+			case '.':
+				if(gopherplus)
+					fputs("+INFO: ", stdout);
+				fputs(buf+1, stdout);
+				printf("\t%s\t%d\t+\r\n", servername, serverport);
+				if(gopherplus) {
+					printf("+ADMIN:\r\n Admin: %s\r\n", adminstring);
+					guessviews(buf+1);
+				}
+				break;
+
+			case '!':
+				cmd = buf+1;
+				if(!strcmp(cmd, "nolist")) {
+					genlist = 0;
+				} else if(!strcmp(cmd, "reverse")) {
+					listrev = 1;
+				} else if(!strcmp(cmd, "mtime")) {
+					dirsortmode = 1;
+				} else {
+					fputs("iInvalid !command in config\t\t\t\r\n", stdout);
+				}
+				break;
+
+			case '#':
+				break;
+
+			default:
+				if(gopherplus)
+					fputs("+INFO: ", stdout);
+				putchar('i');
+				fputs(buf, stdout);
+				fputs("\t-\t-\t-\r\n", stdout);
+				if(gopherplus)
+					printf("+ADMIN:\r\n Admin: %s\r\n", adminstring);
+		}
+	}
 }
 
 /* directory listing */
 void dirlist()
 {
 	FILE *fp;
-	char *p;
-	char buf[REQBUF];
-	char *cmd;
-	int genlist = 1;
-	int listrev = 0;
 	int n, i;
 	struct dirent **namelist;
+
+	if(gopherplus)
+		fputs("+-2\r\n", stdout);
 
 	/* process configuration file */
 	fp = fopen(DIRLIST, "r");
 	if(fp) {
-		while(!feof(fp)) {
-			if(!fgets(buf, REQBUF, fp)) break;
-			for(p=buf; *p; p++)
-				if(*p == '\r' || *p == '\n')
-					{ *p = 0; break; }
-
-			switch(buf[0]) {
-				case ':':
-					fputs(buf+1, stdout);
-					fputs("\t+\r\n", stdout);
-					break;
-
-				case '.':
-					fputs(buf+1, stdout);
-					printf("\t%s\t%d\t+\r\n", servername, serverport);
-					break;
-
-				case '!':
-					cmd = buf+1;
-					if(!strcmp(cmd, "nolist")) {
-						genlist = 0;
-					} else if(!strcmp(cmd, "reverse")) {
-						listrev = 1;
-					} else if(!strcmp(cmd, "mtime")) {
-						dirsortmode = 1;
-					} else {
-						fputs("iInvalid !command in config\t\t\t\r\n", stdout);
-					}
-					break;
-
-				case '#':
-					break;
-
-				default:
-					putchar('i');
-					fputs(buf, stdout);
-					fputs("\t-\t-\t-\r\n", stdout);
-			}
-		}
-
+		readdirlist(fp);
 		fclose(fp);
 	}
 
@@ -223,8 +343,6 @@ void dirlist()
 				printentry(namelist[i]->d_name);
 		}
 	}
-
-	fputs(".\r\n", stdout);
 }
 
 /* serve a file */
@@ -233,9 +351,25 @@ void serve(const char *fn)
 	FILE *fp;
 	int r;
 	char buf[SERVBUF];
+	struct stat stbuf;
+
+	if(iteminfo) {
+		printentry(fn);
+		return;
+	}
+
+	if(stat(fn, &stbuf)) {
+		errormsg("can't stat file");
+		exit(0);
+	}
+
+	if(gopherplus) {
+		printf("+%ld\r\n", stbuf.st_size);
+	}
+
 	fp = fopen(fn, "rb");
 	if(!fp) {
-		printf("3couldn't open file\t\t\t\r\n.\r\n");
+		errormsg("couldn't open file");
 		exit(0);
 	}
 
@@ -250,18 +384,23 @@ void serve(const char *fn)
 void runextern(char *name, char *search)
 {
 	FILE *fp;
+	FILE *fp2;
 	char buf[REQBUF];
 	char *p, *q;
 	char *args[MAXPROCARG];
 	char *exe;
 	int argc=0;
+	int status;
+	int pfd[2];
+	pid_t cpid;
 
 	fp = fopen(EXTPROC, "r");
 	if(!fp) {
-		printf("3Extern not found\t\t\t\r\n.\r\n");
+		errormsg("extern not found");
 		exit(0);
 	}
 
+	/* read .search */
 	while(!feof(fp)) {
 		if(!fgets(buf, REQBUF, fp)) break;
 
@@ -274,6 +413,8 @@ void runextern(char *name, char *search)
 		*q = 0;
 		if(strcmp(p, name)) continue;
 
+		/* found entry, built argument list */
+
 		p=q+1;
 
 		exe = p;
@@ -283,20 +424,74 @@ void runextern(char *name, char *search)
 			*q=0;
 
 			args[argc] = p; argc++;
-			if(argc == MAXPROCARG - 1) {
-				printf("3Too many arguments for extern\t\t\t\r\n.\r\n");
+			if(argc == MAXPROCARG - 2) {
+				errormsg("too many arguments for extern");
 				exit(0);
 			}
 		}
 
 		args[argc] = search; argc++;
+
+		if(gopherplus && !managedextern) {
+			args[argc] = "$"; argc++;
+		}
+
 		args[argc] = NULL;
 
-		execve(exe, args, genvp);
+
+		/* invoke processor */
+
+		if(managedextern) {
+			/* execute managed processor */
+
+			if(pipe(pfd) == -1) {
+				perror("pipe");
+				exit(1);
+			}
+
+			cpid = fork();
+			if(cpid == -1) {
+				perror("fork");
+				exit(1);
+			}
+
+			if(cpid == 0) {
+				/* child */
+				close(pfd[0]); /* close unused read end */
+				close(1); /* close stdout */
+				dup2(pfd[1], 1); /* assign writing end to stdout */
+				execve(exe, args, genvp);
+				errormsg("error running extern");
+				exit(1);
+			} else {
+				/* parent */
+				close(pfd[1]); /* close unused write end */
+
+				if(gopherplus)
+					fputs("+-2\r\n", stdout);
+
+				fp2 = fdopen(pfd[0], "r");
+				readdirlist(fp2);
+				fclose(fp2);
+
+				waitpid(cpid, &status, WNOHANG);
+				if(!WIFEXITED(status)) {
+					kill(cpid, 9); /* kthxbye */
+				}
+			}
+
+			exit(0);
+
+		} else {
+			/* unmanaged */
+			execve(exe, args, genvp);
+			errormsg("error running extern");
+			exit(1);
+		}
 	}
 
 	fclose(fp);
-	printf("3Extern not found\t\t\t\r\n.\r\n");
+	errormsg("extern not found");
 	exit(0);
 }
 
@@ -305,13 +500,28 @@ void procreq(char *req)
 {
 	char *pathel;
 	char *search = NULL;
-	char *sep;
+	char *sep, *p;
 	struct stat stbuf;
 	node *no = NULL;
 
 	if((sep = strchr(req, '\t'))) {
+		/* check for search string / gopher+ flags */
 		search = sep+1;
-		*sep=0;
+		*sep = 0;
+
+		p = strchr(search, '\t');
+		if(p) {
+			*p = 0;
+			p++;
+		} else p = search;
+
+		if(!strcmp(p, "!")) {
+			iteminfo = 1;
+			gopherplus = 1;
+		} else if(!strcmp(p, "$"))
+			gopherplus = 1;
+		else if(p[0] == '+')
+			gopherplus = 1;
 	}
 
 	if(!strncmp(req, "URL:", 4)) {
@@ -325,21 +535,23 @@ void procreq(char *req)
 		exit(0);
 	}
 
+	/* follow path */
 	while((sep = strchr(req, '/'))) {
 		pathel = req;
 		*sep = 0;
 		req = sep + 1;
 
 		if(!strcmp(pathel, "..")) {
-			printf("3invalid path\t\t\t\r\n.\r\n");
+			errormsg("invalid path");
 			exit(0);
 		}
 
 		if(chdir(pathel)) {
-			printf("3'%s' not found\t\t\t\r\n.\r\n", pathel);
+			errormsg("file not found or chdir failed");
 			exit(0);
 		}
 
+		/* record path */
 		if(no) {
 			no->next = mkpn(pathel);
 			no = no->next;
@@ -348,22 +560,33 @@ void procreq(char *req)
 		}
 	}
 
+	/* final path element */
 	if(req[0]) {
-		if(req[0] == '!') {
+		if(req[0] == '!' || req[0] == '@') {
+			/* external processor */
+			if(req[0] == '@') managedextern = 1;
 			runextern(req+1, search);
 		}
 
 		if(stat(req, &stbuf)) {
-			printf("3not found\r\n.\r\n");
+			errormsg("file not found");
+			exit(0);
+		}
+
+		if(iteminfo) {
+			/* instead of serving, print info */
+			printentry(req);
 			exit(0);
 		}
 
 		if(S_ISDIR(stbuf.st_mode)) {
+			/* directory */
 			if(chdir(req)) {
-				printf("3'%s' not found\t\t\t\r\n.\r\n", req);
+				errormsg("chdir failed");
 				exit(0);
 			}
 
+			/* record path */
 			if(no) {
 				no->next = mkpn(req);
 				no = no->next;
@@ -376,11 +599,12 @@ void procreq(char *req)
 		}
 
 		if(S_ISREG(stbuf.st_mode)) {
+			/* serve file */
 			serve(req);
 			exit(0);
 		}
 
-		printf("3not regular file\t\t\t\r\n.\r\n");
+		errormsg("not regular file");
 		exit(0);
 	}
 
@@ -393,9 +617,15 @@ int main(int argc, char *argv[], char *envp[])
 	char buf[REQBUF];
 	char *p;
 	int o;
+	time_t tm;
+	struct tm *mt;
 
-	while((o = getopt(argc, argv, "n:p:r:h")) != -1) {
+	/* process arguments */
+	while((o = getopt(argc, argv, "n:p:r:a:l:h")) != -1) {
 		switch(o) {
+			case 'a':
+				adminstring = strdup(optarg);
+				break;
 			case 'n':
 				servername = strdup(optarg);
 				break;
@@ -405,12 +635,17 @@ int main(int argc, char *argv[], char *envp[])
 			case 'r':
 				rootdir = strdup(optarg);
 				break;
+			case 'l':
+				logfile = strdup(optarg);
+				break;
 			case '?':
 			case 'h':
 				fprintf(stderr, "mgod mini gopher server\n\n");
-				fprintf(stderr, "-n: set server name (default 127.0.0.1)\n");
-				fprintf(stderr, "-p: set server port (default 70)\n");
-				fprintf(stderr, "-r: set root dir (default /var/gopher)\n");
+				fprintf(stderr, "-n name: set server name (default 127.0.0.1)\n");
+				fprintf(stderr, "-p port: set server port (default 70)\n");
+				fprintf(stderr, "-r dir: set root dir (default /var/gopher)\n");
+				fprintf(stderr, "-a str: set admin string\n");
+				fprintf(stderr, "-l name: enable logging\n");
 				exit(1);
 		}
 	}
@@ -428,6 +663,13 @@ int main(int argc, char *argv[], char *envp[])
 		perror("couldn't change to root dir");
 		exit(1);
 	}
+
+	tm = time(NULL);
+	mt = localtime(&tm);
+	logprintf("REQ %04d-%02d-%02d %02d:%02d:%02d '%s'\n",
+			mt->tm_year + 1900, mt->tm_mon + 1, mt->tm_mday,
+			mt->tm_hour, mt->tm_min, mt->tm_sec,
+			buf);
 
 	genvp = envp;
 	procreq(buf);
